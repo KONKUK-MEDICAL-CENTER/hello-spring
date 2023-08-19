a8966c09dda86c83642a3aedb87b87b2d17efd0b
sonymaker <sonymaker@naver.com>
2023-08-19 21:18:19 +0900

    spring-boot 3.2.1(spring web, thymeleaf) 세팅

---
+++ src/test/java/kr/ac/kuh/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kuh.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
